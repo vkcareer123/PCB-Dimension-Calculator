125d5e05e5410960174549c111980d29dc7a832c
vkcareer123 <vkcareer123@gmail.com>
2026-07-17 11:01:58 +0530

    GerberParser.php

--- app/Services/Gerber/GerberParser.php
+++ app/Services/Gerber/GerberParser.php
@@ -2,11 +2,32 @@
 
 namespace App\Services\Gerber;
 
+use Exception;
 use Illuminate\Support\Facades\File;
 
 class GerberParser
 {
     public function parse(string $directory): array
+    {
+        // Prefer .gbrjob if available
+        if ($jobFile = $this->findGerberJob($directory)) {
+            $data = json_decode(File::get($jobFile->getPathname()), true);
+
+            if (
+                isset($data['GeneralSpecs']['Size']['X']) &&
+                isset($data['GeneralSpecs']['Size']['Y'])
+            ) {
+                return [
+                    'width'  => round((float) $data['GeneralSpecs']['Size']['X'], 2),
+                    'height' => round((float) $data['GeneralSpecs']['Size']['Y'], 2),
+                ];
+            }
+        }
+
+        return $this->parseEdgeCuts($directory);
+    }
+
+    private function parseEdgeCuts(string $directory): array
     {
         $file = $this->findEdgeCuts($directory);
 
@@ -41,27 +62,40 @@ class GerberParser
         }
 
         if ($minX === PHP_FLOAT_MAX) {
-            throw new \Exception('Unable to determine PCB dimensions.');
+            throw new Exception('Unable to determine PCB dimensions.');
         }
 
         return [
-            'width' => round($maxX - $minX, 2),
+            'width'  => round($maxX - $minX, 2),
             'height' => round($maxY - $minY, 2),
         ];
+    }
+
+    private function findGerberJob(string $directory): ?\SplFileInfo
+    {
+        foreach (File::allFiles($directory) as $file) {
+            if (strtolower($file->getExtension()) === 'gbrjob') {
+                return $file;
+            }
+        }
+
+        return null;
     }
 
     private function findEdgeCuts(string $directory)
     {
         foreach (File::allFiles($directory) as $file) {
 
+           $content = File::get($file->getPathname());
+
             if (
                 str_contains($file->getFilename(), 'Edge_Cuts') ||
-                str_contains(File::get($file), 'FileFunction,Profile')
+                str_contains($content, 'FileFunction,Profile')
             ) {
                 return $file;
             }
         }
 
-        throw new \Exception('PCB outline not found. Please make sure your Gerber ZIP contains the Edge_Cuts layer.');
+        throw new Exception('PCB outline not found. Please make sure your Gerber ZIP contains the Edge_Cuts layer.');
     }
 }
